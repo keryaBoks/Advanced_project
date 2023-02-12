347a4334e14d8157bd6ea41304347d9358d352b3
Kirill <k.danchenko@pixelplex.io>
2023-02-12 13:50:29 +0300

    Create repo

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,6 @@
+/* eslint-disable i18next/no-literal-string */
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
